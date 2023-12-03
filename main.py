80805f750edc2d886f4f753b91f353e558c61f2f
scottxwalsh <scottxwalsh@gmail.com>
2023-12-03 18:38:57 -0500

    Including main.py and requirements.txt

---
+++ main.py
@@ -0,0 +1,18 @@
+while True:
+    print("\nExpense Tracker")
+    print("1. Add Expense")
+    print("2. View Expenses")
+    print("3. Exit")
+    choice = input("Enter your choice: ")
+
+    if choice == "1";
+        # Add Expense
+        pass
+    elif choice == "2";
+        # View Expenses
+        pass
+    elif choice == "3";
+        print("Exiting the program")
+        break
+    else:
+        print("Invalid choice. Please try again.")
